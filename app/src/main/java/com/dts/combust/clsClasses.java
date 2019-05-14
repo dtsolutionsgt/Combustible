edfec105ab5e4123c092becc3472324bdc1a8928
DTSolutions <dtsolutionsgt@gmail.com>
2019-05-14 13:48:33 -0600

    Initial commit

---
+++ app/src/main/java/com/dts/combust/clsClasses.java
@@ -0,0 +1,24 @@
+package com.dts.combust;
+
+public class clsClasses {
+
+    public class clsUsuario {
+        public int id;
+        public String nombre;
+        public int activo;
+        public String login;
+        public String clave;
+        public int rol;
+    }
+
+    public class clsRol {
+        public int  id;
+        public String nombre;
+    }
+
+    public class clsMenu {
+        public int  id;
+        public String nombre;
+    }
+
+}
